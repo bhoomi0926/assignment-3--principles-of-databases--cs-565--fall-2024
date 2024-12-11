a71c12c8f16dbc4b2b64293dfac03bf2bfe3725e
bhoomi0926 <bhoomikaashok007@gmail.com>
2024-12-10 20:15:12 -0500

    refine update and delete routes with validation

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ nunjucks.configure(`views`, {
 });
 
 /*
- * Configure the Node MongoDB client to connect to Mongo, establish a database
- * connection, and assign the reference to the “db” variable defined on line
+ * Configure the Node MongoDB client to connect to Mongo, established a database
+ * connection and assigning the reference to the “db” variable defined on line
  * 21
  */
 mongoClient.connect(`${dbURL}:${dbPort}`, (err, client) => {
@@ -42,7 +42,8 @@ mongoClient.connect(`${dbURL}:${dbPort}`, (err, client) => {
         console.log(`MongoDB successfully connected:`);
         console.log(`\tMongo URL:`, colors.green, dbURL, colors.reset);
         console.log(`\tMongo port:`, colors.green, dbPort, colors.reset);
-        console.log(`\tMongo database name:`, colors.green, dbName, colors.reset, `\n`);
+       console.log(`\tMongo database name:`,
+            colors.green, dbName, colors.reset, `\n`);
     }
 });
 
@@ -66,7 +67,7 @@ app.set(`view engine`, `njk`);
  * Express’s middleware to parse incoming, form-based request data before
  * processing form data
  */
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 /*
  * Express’s middleware to parse incoming request bodies before handlers
@@ -81,31 +82,42 @@ app.use(bodyParser.json());
 app.use(express.static(`public`));
 
 /*
+ * Note:
+ *   — “req” stands for requests, which arrive from the client/browser
+ *   — “res” stands for responses, which are sent to the client/browser
+ */
+
+/*
  * This router handles GET requests to the root of the web site
  */
 app.get(`/`, (req, res) => {
-    console.log(`User requested root of web site.`);
-    console.log(`Responding to request with file`, colors.green, `index.njk`, colors.reset, `via GET.`);
-    res.render(`index.njk`);
+  console.log(`User requested root of web site.`);
+  console.log(`Responding to request with file`,
+      colors.green, `index.njk`, colors.reset, `via GET.`);
+
+  res.render(`index.njk`);
 });
 
 /*
  * This router handles GET requests to http://localhost:3000/read-a-db-record/
  */
 app.get(`/read-a-db-record`, (req, res) => {
-    db.collection(dbCollection).find().toArray((err, arrayObject) => {
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log(`User requested http://${HOST}:${port}/read-a-db-record.`);
-            console.log(`Responding to request with file`, colors.green, `read-from-database.njk`, colors.reset, `via GET.\n`);
-            res.render(`read-from-database.njk`, { mongoDBArray: arrayObject });
-        }
-    });
+  db.collection(dbCollection).find().toArray((err, arrayObject) => {
+      if (err) {
+          return console.log(err);
+      } else {
+          console.log(`User requested http://${HOST}:${port}/read-a-db-record.`);
+          console.log(`Responding to request with file`,
+              colors.green, `read-from-database.njk`, colors.reset, `via GET.\n`);
+
+          res.render(`read-from-database.njk`, {mongoDBArray: arrayObject});
+      }
+  });
 });
 
 /*
- * This router handles GET requests to http://localhost:3000/create-a-db-record/
+ * This router handles GET requests to
+ * http://localhost:3000/create-a-db-record/
  */
 app.get(`/create-a-db-record`, (req, res) => {
     res.render(`create-a-record-in-database.njk`);
